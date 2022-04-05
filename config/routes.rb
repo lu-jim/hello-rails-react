a1cc8b6e542a44ff56a7d894e8917952b7d27a63
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-05 14:38:37 -0400

    Create static view and configure basic react setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        get 'messages/index'
+      end
+    end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "static#index"
 end
