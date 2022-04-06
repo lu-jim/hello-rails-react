2cfdffe0243a0c0822726945f84c951cdb805446
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-04-06 19:05:09 -0400

    Delete mock dependencies

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,8 +2,6 @@ import React from "react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
-import PropTypes from "prop-types"
-import HelloWorld from "./HelloWorld";
 import Greeting from "./Greeting";
 
 
@@ -12,7 +10,6 @@ const App = () => (
     <Router>
         <Routes>
           <Route exact path="/" element={<h1> Home</h1> } />
-          <Route path="/hello" element={<HelloWorld greeting="Hola panas" />} />
           <Route path="/message" element={<Greeting />} />
         </Routes>
     </Router>
